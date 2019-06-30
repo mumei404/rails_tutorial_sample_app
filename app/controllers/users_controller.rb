ec29cb05b8e4c1b63880d78ccb79adb55a139471
shimazaki <lozld30@gmail.com>
2019-06-30 16:31:07 +0900

    create edit and update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,18 @@ class UsersController < ApplicationController
 		end
 	end
 
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	def update
+		@user = User.find(params[:id])
+		if @user.update_attributes(user_params)
+		else
+			render 'edit'
+		end
+	ena
+
 	def show
 		@user = User.find(params[:id])
 	end
